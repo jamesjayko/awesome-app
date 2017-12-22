3b43531378695ca5a17d7caf23edea5b973a8a10
jay <jamesjayko@gmail.com>
2017-12-22 12:36:23 -0800

    finished react setup

---
+++ client/src/components/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default props => {
+  return (
+    <div>
+      <h1>This is home!</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+        quibusdam saepe iure excepturi.
+      </p>
+    </div>
+  );
+};
